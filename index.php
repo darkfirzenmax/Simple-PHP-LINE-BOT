178c5269b1c4d4bc6ae518864e5381e648ff8bd0
darkfirzenmax <37853270+darkfirzenmax@users.noreply.github.com>
2019-09-24 17:47:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,31 +4,18 @@ require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 $text = $bot->getMessageText();
 $bot->reply('==>'.$text);
-$url = 'https://testlab.firstohm.com.tw/index.php';
 
-    $fields = array(
-       'app'=>'users',
-'format'=>'raw',
-'resource'=>'userinfo',
-'option'=>'com_api',
-'key'=>'ef280bf8341539515115a5514249cba8',
-'linemsg'=>'$text'
-    );
+$url = 'https://testlab.firstohm.com.tw/index.phpp?app=users&format=raw&resource=userinfo&option=com_api&key=ef280bf8341539515115a5514249cba8&linemsg=$text';
 
-    // build the urlencoded data
-    $postvars = http_build_query($fields);
 
-    // open connection
-    $ch = curl_init();
+  $opts = array('http' =>
+    array(
+      'method' => 'POST',
+      'header' => 'Content-type: application/x-www-form-urlencoded'
+    )
+  );
 
-    // set the url, number of POST vars, POST data
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, count($fields));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+  $context = stream_context_create($opts);
 
-    // execute post
-    $result = curl_exec($ch);
-
-    // close connection
-    curl_close($ch);
+  $result = file_get_contents($url, false, $context);
 ?>
