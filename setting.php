20a742e3656ec69c520fa4d839f8767ee83df865
darkfirzenmax <37853270+darkfirzenmax@users.noreply.github.com>
2019-09-24 15:50:26 +0800

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,11 +2,11 @@
 
 class Setting {
 	public static function getChannelAccessToken(){
-		$channelAccessToken = "";
+		$channelAccessToken = "3pjYn+6gs3G1dISd6w8Xb8vCOIWiz5ojncvzdVB1jV24nSASa/walrkRhrFMU4kqaRBTZQOZ7S3SplRGUMVfk7sdkYHb1bfgpRuGSoRciWKbGNqhnKDddNPD4pn2ARd8rFG92itQIY+mqjGuLOGmO1GUYhWQfeY8sLGRXgo3xvw=";
 		return $channelAccessToken;
 	}
 	public static function getChannelSecret(){
-		$channelSecret = "";
+		$channelSecret = "1b8749dd96ed6430bd43c094692ecd8d";
 		return $channelSecret;
 	}
 	public static function getApiReply(){
